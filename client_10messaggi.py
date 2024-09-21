457da1a962e4df813888ab5cfcbfc87158c317d6
FilippoMeinero <filippo.meinero@itiscuneo.eu>
2024-09-21 11:56:48 +0200

    Sending 10 messages client-server

---
+++ client_10messaggi.py
@@ -0,0 +1,19 @@
+import socket
+
+num_messaggi = 10
+
+# configurazione del client
+server_address = ("192.168.1.6", 6980) # indirizzo ip e porta in una tupla
+BUFFER_SIZE = 4092  # numero di byte massimo che posso mandare o ricevere
+
+# creazione del socket UDP
+udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    # DGRAM --> protocollo UDP
+
+# Invio di dieci messaggi
+for i in range(num_messaggi):
+    message = (f"Messaggio n {i+1} dal CLIENT")
+    udp_client_socket.sendto(message.encode(), server_address)
+    data, address = udp_client_socket.recvfrom(BUFFER_SIZE)
+    print(f'Messaggio ricevuto: {data.decode()} da {address}')  # decodifica dei dati
+
+udp_client_socket.close()
---
+++ server_10messaggi.py
@@ -0,0 +1,25 @@
+import socket
+
+num_messaggi = 10
+
+# configurazione server
+server_address = ("192.168.1.6", 6980) # indirizzo ip e porta in una tupla
+BUFFER_SIZE = 4092  # numero di byte massimo che posso mandare o ricevere
+
+# creazione socket UDP
+udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)    # DGRAM --> protocollo UDP
+udp_server_socket.bind(server_address) 
+
+
+print("Server UDP in ascolto")
+
+# loop per ricevere messaggi
+for _ in range(num_messaggi):
+    data, address = udp_server_socket.recvfrom(BUFFER_SIZE) # BLOCCANTE (è in ascolto)
+    # address --> indirizzo che fa la richiesta
+    # data --> dati inviati da quell'indirizzo
+
+    print(f"Messaggio ricevuto da {address}: {data.decode()}")
+    udp_server_socket.sendto(b"Messaggio ricevuto", address)    # b --> indica che è una stringa di byte
+
+udp_server_socket.close()
